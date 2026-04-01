c283bfab81ac8ed54076e2d82b700eb8630397cc
Nahu <125503938+NahuelBarria@users.noreply.github.com>
2026-04-01 12:46:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const cursor = document.getElementById('cursor');
 
-document.addEventListener('mousemove', e => {
-  cursor.style.left = e.clientX + 'px';
-  cursor.style.top = e.clientY + 'px';
-});
-
-document.querySelectorAll('a').forEach(el => {
-  el.addEventListener('mouseenter', () => cursor.classList.add('hover'));
-  el.addEventListener('mouseleave', () => cursor.classList.remove('hover'));
-});
-
-const rocketBtn = document.getElementById('rocketBtn');
-
-window.addEventListener('scroll', () => {
-  rocketBtn.classList.toggle('visible', window.scrollY > 200);
-});
